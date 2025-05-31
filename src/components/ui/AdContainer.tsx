fec765255dd814961570c8a215471e4e4c465b08
nextkaki <idrgf150@naver.com>
2025-05-31 16:20:03 +0900

    250531 :: 컴포넌트 에러 수정.

--- src/components/ui/AdContainer.tsx
+++ src/components/ui/AdContainer.tsx
@@ -10,15 +10,15 @@ export default function AdContainer({ size = 'vertical', className = '' }: AdCon
   switch (size) {
     case 'vertical':
       dimensions = 'h-[600px] w-[300px]';
-      text = '광고 영역 기다립니다.';
+      text = '광고 영역.';
       break;
     case 'horizontal':
       dimensions = 'h-[90px] w-full';
-      text = '광고 영역 기다립니다.';
+      text = '광고 영역.';
       break;
     case 'square':
       dimensions = 'h-[250px] w-[300px]';
-      text = '광고 영역 기다립니다.';
+      text = '광고 영역.';
       break;
   }
   
